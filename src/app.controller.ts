9cd475e78717eda0b57353aaca820bc81e6480dc
Tarcio Rodrigo <tr.rodrigo@gmail.com>
2020-10-27 08:04:15 -0300

    interceptors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Logger } from '@nestjs/common';
-import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
+import { Ctx, EventPattern, MessagePattern, Payload, RmqContext } from '@nestjs/microservices';
 import { AppService } from './app.service';
 import { Categoria } from './interfaces/categorias/categoria.interface';
+
+const ackErrors: string[] = ['E11000'];
 
 @Controller()
 export class AppController {
@@ -11,11 +13,26 @@ export class AppController {
 
   @EventPattern('criar-categoria')
   async criarCategoria(
-    @Payload() categoria: Categoria) {
+    @Payload() categoria: Categoria, @Ctx() context: RmqContext) {
+
+      const channel = context.getChannelRef();
+      const originalMsg = context.getMessage();
 
       this.logger.log(`categoria: ${JSON.stringify(categoria)}`);
 
-      await this.appService.criarCategoria(categoria);
+      try {
+        await this.appService.criarCategoria(categoria);
+        await channel.ack(originalMsg);
+      } catch(error) {
+        this.logger.error(`error: ${JSON.stringify(error.message)}`)
+
+        ackErrors.map(async ackError => {
+            if (error.message.includes(ackError)) {
+              await channel.ack(originalMsg);
+            }
+          }
+        )
+      }
 
   }
 
--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
     transport: Transport.RMQ,
     options: {
       urls: ['amqp://user:60lO7CUob613@34.203.216.219:5672/smartranking'],
+      noAck: false,
       queue: 'admin-backend'
     }
   });
